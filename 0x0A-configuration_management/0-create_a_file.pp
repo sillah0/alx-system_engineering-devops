d3eef6edf00137bdca202cf7149f7615023c451c
sillah0 <siladennis624@gmail.com>
2024-05-03 23:40:47 +0300

    fixing indent errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Puppet manifest used to create a file with \tmp
 
-file {'/tmp/school':
-mode => '0744',
-owner => 'www-data'.
-group => 'www-data',
-content => 'I love Puppet',
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
